b2616bd8631aa855b50a5fbb9ab0c6591466b94e
applesalsaa <znio2862@gmail.com>
2026-04-08 10:22:34 -0700

    Committing changes for step 5 of IA2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -24,7 +24,7 @@ const users = {
     {
       id: "yat999",
       name: "Dee",
-      job: "Aspring actress"
+      job: "Aspiring actress"
     },
     {
       id: "zap555",
@@ -34,14 +34,23 @@ const users = {
   ]
 };
 
+const findUserById = (id) => 
+    users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send(users);
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
 });
 
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
-  );
+);
 });
